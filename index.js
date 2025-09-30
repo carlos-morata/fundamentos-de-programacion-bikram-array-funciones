b87e3f411208537830f7e28d3da93deca6c48458
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2025-10-01 00:16:42 +0200

    add/ej/5,6

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ let arrayNumerosPares = [0, 2, 4, 6, 8];
 
 // 4
 let arrayBidimensional = [[0, 1, 2], ['a', 'b', 'c']];
+
+// 5 FUNCIONES
+function suma(numero1, numero2) {
+    let suma = numero1 + numero2;
+    return suma;
+}
+console.log(suma(15, 10));
+
+// 6
+function potenciacion(numero1, numero2) {
+    let resultado = numero1^numero2;
+}
+console.log(potenciacion(48, 9));
