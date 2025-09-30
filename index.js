00586cdbc168641d42893a1005cd8a1ac1c39608
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2025-10-01 00:07:59 +0200

    add/ej/1, 2, 3, 4

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello bikram");
+// 1 ARRAYS
+let arrayVacio = [];
+
+// 2
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// 3
+let arrayNumerosPares = [0, 2, 4, 6, 8];
+
+// 4
+let arrayBidimensional = [[0, 1, 2], ['a', 'b', 'c']];
